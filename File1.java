89955184e022e438f1446441e6f0098b6599bc0f
Venkatesh49 <86230485+Venkatesh49@users.noreply.github.com>
2024-05-16 14:38:43 +0530

    Update File1.java
    var aded

--- File1.java
+++ File1.java
@@ -2,9 +2,11 @@ import java.lang.*;
 
 public class File1
 {
+  int X=100;
 public ststic void main(String[]args){
 System.out.println("FirstnJava");
 System.out.println("SecondLine of Code");
+  
 }
 }
 
