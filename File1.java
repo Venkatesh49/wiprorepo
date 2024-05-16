3edac3c3e4a53f708d9ba0ae40da72ef8e739a4e
Venkatesh49 <venkateshatla49@gmail.com>
2024-05-16 22:41:00 +0530

    updated java file

--- File1.java
+++ File1.java
@@ -4,15 +4,14 @@ import java.lang.*;
 public class File1
 {
 
+  
   int X=100;
   int y=10;
-  int z=100;
   public static void main(String[]args)
   {
       System.out.println("FirstnJava");
       System.out.println("SecondLine of Code");
-      System.out.println("thirds line");
-      System.out.println("Fourth line");
+      
   }
 }
 
