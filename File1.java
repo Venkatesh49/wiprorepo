bb8bcf0c9ffcd5a5869f8dc07a7d8e9bf56e257f
Venkatesh49 <venkateshatla49@gmail.com>
2024-05-16 22:18:39 +0530

    final changed made

--- File1.java
+++ File1.java
@@ -1,23 +1,19 @@
+
 import java.lang.*;
 
 public class File1
 {
 
   int X=100;
-
   int y=10;
-<<<<<<< HEAD
-
-=======
   int z=100;
->>>>>>> 929bdbf17c08f21a23a40f1cc328fb0e578cd88f
-public static void main(String[]args){
-  
-System.out.println("FirstnJava");
-System.out.println("SecondLine of Code");
-  System.out.println("thirds line");
-  System.out.println("Fourth line");
-}
+  public static void main(String[]args)
+  {
+      System.out.println("FirstnJava");
+      System.out.println("SecondLine of Code");
+      System.out.println("thirds line");
+      System.out.println("Fourth line");
+  }
 }
 
 
