eff9e37b43e7e81ca6e2abec92918ac655e31a40
Venkatesh49 <86230485+Venkatesh49@users.noreply.github.com>
2024-05-16 14:45:20 +0530

    Update File1.java
    var added again

--- File1.java
+++ File1.java
@@ -3,6 +3,7 @@ import java.lang.*;
 public class File1
 {
   int X=100;
+  int y=10;
 public static void main(String[]args){
 System.out.println("FirstnJava");
 System.out.println("SecondLine of Code");
